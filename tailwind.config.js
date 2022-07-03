a2d8d3bd8c53ed197882c29277e01659247389bc
arth <arththapa7@gmail.com>
2022-07-03 16:53:02 +0530

    Init

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+        poorStory: ['Poor Story', 'cursive'],
+        DMsans: ['DM Sans', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
