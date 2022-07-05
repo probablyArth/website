0dd018e6f5446163b0dcb4ca72b6241c0db324d2
ArthThapa <arththapa7@gmail.com>
2022-07-05 22:47:58 +0530

    better Hero

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -1,11 +1,11 @@
 /* eslint-disable @next/next/no-img-element */
 const Hero = () => {
   return (
-    <div className="flex justify-between items-center flex-col md:flex-row">
+    <div className="flex justify-between items-center flex-col-reverse md:flex-row">
       <h2>
         Hi 👋, My name is <span>Arth</span> I build fullstack webapps
       </h2>{" "}
-      <img src="/avatar.svg" alt="" className="avatar" />
+      <img src="/avatar.svg" alt="" className="avatar max-w-xs" />
     </div>
   );
 };
