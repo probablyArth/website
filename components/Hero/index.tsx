cef4450dce668009f8735177db3d76b28ff5f3fa
ArthThapa <arththapa7@gmail.com>
2022-07-06 22:13:54 +0530

    I just want to commit man :cry:

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -2,7 +2,7 @@
 const Hero = () => {
   return (
     <div className="flex justify-between items-center flex-col-reverse md:flex-row">
-      <h2>
+      <h2 className="text-2xl">
         Hi 👋, My name is <span>Arth</span> I build fullstack webapps
       </h2>{" "}
       <img src="/avatar.svg" alt="" className="avatar max-w-xs" />
