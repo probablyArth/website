8ce8155a6549220426073874847cb6766be2e53f
ArthThapa <arththapa7@gmail.com>
2022-07-08 14:05:45 +0530

    Multi region

--- main/next.config.js
+++ main/next.config.js
@@ -1,6 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+
+const { BLOG_URL } = process.env;
+
+module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: "/:path*",
+        destination: `/:path*`,
+      },
+      {
+        source: "/blog",
+        destination: `${BLOG_URL}/`,
+      },
+      {
+        source: "/blog/:path*",
+        destination: `${BLOG_URL}/:path*`,
+      },
+    ];
+  },
+};
